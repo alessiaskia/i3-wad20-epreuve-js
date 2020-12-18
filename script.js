284fb0e746b9994a3d48efdf5add7895ca7b4dbc
alessiaskia <a.scaccia@interface3.be>
2020-12-18 11:00:05 +0100

    added basic CSS

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const app = document.getElementById('app');
 const title = '<h1>Précommande de vaccins contre la COVID-19</h1>';
 
 // create header w/2 buttons
-const header = '<header><button class="btn-filter-by-price">Filtrer par prix</button><button class="btn-filter-by-approval">Cacher les vaccins non approuvés</button></header>';
+const header = '<header><button class="btn-filter-by-price btn-outline-primary">Filtrer par prix</button><button class="btn-filter-by-approval btn-outline-primary">Cacher les vaccins non approuvés</button></header>';
 
 // create card for each vaccine
 let card = '';
-let container = '<main class="flex-wrap">';
+let container = '<main class="wrapper">';
 
 for (const vaccine of vaccines) {
   card = `<div class="card" id="${vaccine.id}">
@@ -30,17 +30,20 @@ for (const vaccine of vaccines) {
   } else {
     card += '<li class="list-group-item">Approuvé : non</li>';
   }
-  card += `</ul>
-        <label for="quantity">Quantity : </label>
-        <input type="number" id="quantity">
-        <button class="btn-book">Réserver </button>
-        </div></div>`;
+  card += `<li class="list-group-item">
+            <label for="quantity">Quantity : </label>
+            <input type="number" id="quantity">
+            <button class="btn-book btn-primary btn-sm">Réserver </button>
+            </li>
+        </ul>
+    </div>
+    </div>`;
   container += card;
 }
 container += '</main>';
 
 // create footer
-const footer = '<footer><h3>Résumé de votre commande</h3><button class="btn-order">Passer la commande</button></footer>';
+const footer = '<footer><h3>Résumé de votre commande</h3><button class="btn-order btn-primary btn-lg">Passer la commande</button></footer>';
 
 // --> to HTML
 app.innerHTML = `${title + header + container + footer}`;
