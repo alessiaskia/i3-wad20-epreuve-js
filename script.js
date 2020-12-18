ff7241fe5ae31735f838df60e3a686820e23a359
alessiaskia <a.scaccia@interface3.be>
2020-12-18 16:40:59 +0100

    final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 import './styles.scss';
 import { vaccines } from './src/data';
 
@@ -38,16 +39,14 @@ for (const vaccine of vaccines) {
             <li class="list-group-item">Quantité : ${vaccine.quantity}</li>
             <li class="list-group-item">Prix unitaire : ${vaccine.price}</li>`;
   if (vaccine.approved) {
-    card += '<li class="list-group-item">Approuvé : oui</li>';
+    card += '<li class="list-group-item">Approuvé : oui</li></ul>';
   } else {
-    card += '<li class="list-group-item">Approuvé : non</li>';
+    card += '<li class="list-group-item">Approuvé : non</li></ul>';
   }
-  card += `<li class="list-group-item">
+  card += `<div class="card-footer">
             <label for="quantity">Quantity : </label>
             <input type="number" id="quantity" name="quantity" min="1" max="${vaccine.quantity}">
-            </li>
-            </ul>
-        <input type="submit" value="Réserver" class="btn-book btn-primary btn-sm">
+        <input type="submit" value="Réserver" class="btn-book btn-primary btn-sm"></div>
     </div>
     </div>`;
   container += card;
@@ -55,13 +54,15 @@ for (const vaccine of vaccines) {
 container += '</main>';
 
 // create footer
-const footer = '<footer><h3>Résumé de votre commande</h3><div class="booking-area"></div><button class="btn-order btn-primary btn-lg">Passer la commande</button></footer>';
+const footer = '<footer><h3>Résumé de votre commande</h3><div class="booking-area"></div><div class="cancel-area"></div><button class="btn-order btn-primary btn-lg">Passer la commande</button></footer>';
 
 // --> to HTML
 app.innerHTML = `${title + header + container + footer}`;
 
 // delegation
 const vaccineCards = document.querySelectorAll('.card');
+const bookingArea = document.querySelector('.booking-area');
+const cancelArea = document.querySelector('.cancel-area');
 
 document.body.addEventListener('click', (e) => {
   // show/hide non-approved vaccines
@@ -78,28 +79,43 @@ document.body.addEventListener('click', (e) => {
     }
     // actions when user clicks on "réserver"
   } else if (e.target.matches('.btn-book')) {
-    const selectedVaccine = e.target.parentNode.parentNode;
+    const selectedVaccine = e.target.parentNode.parentNode.parentNode;
+    console.log(selectedVaccine);
     const vaccine = vaccines[selectedVaccine.id];
     const chosenVaccine = vaccine.name;
+    const maxQuantity = vaccine.quantity;
     const wantedQuantity = e.target.parentNode.querySelector('#quantity').value;
-    const bookingArea = document.querySelector('.booking-area');
-    const inputArea = e.target.parentNode.children[1].children[6];
+    const inputArea = e.target.parentNode.children[1];
 
-    // put selected vaccine + desired quantity in footer
-    bookingArea.innerHTML += `<p>${chosenVaccine} - quantité : ${wantedQuantity}`;
+    // prevent user to select more vaccines than available
+    if (wantedQuantity <= maxQuantity) {
+      // put selected vaccine + desired quantity in footer
+      bookingArea.innerHTML += `<p>${chosenVaccine} - quantité : ${wantedQuantity}`;
 
-    // hide input area
-    inputArea.classList.add('invisible');
+      // hide input area
+      inputArea.classList.add('invisible');
 
-    // disable booking button
-    e.target.disabled = true;
+      // disable booking button
+      e.target.disabled = true;
+    } else {
+      alert(`Vous pouvez commander max ${maxQuantity} vaccins`);
+    }
+
+    // create button to cancel booking
+    const buttonCancel = '<button id="btn-cancel-booking">Annule la commande</button>';
+    cancelArea.innerHTML += buttonCancel;
 
     // confirm booking
   } else if (e.target.matches('.btn-order')) {
-    console.log(e.target);
-    app.innerHTML = '<h2>La commande a bien été enregistrée</h2>';
+    // only if booking area contains something
+    if (bookingArea.innerHTML.length > 0) {
+      app.innerHTML = '<h2>La commande a bien été enregistrée</h2>';
+    } else {
+      alert('Votre commande est vide');
+    }
+    // cancel the booking in progress (incomplete)
+  } else if (e.target.matches('#btn-cancel-booking')) {
+    bookingArea.innerHTML = '';
+    cancelArea.innerHTML = '';
   }
 });
-
-window.console.log('coucou');
-console.log('coucou');
